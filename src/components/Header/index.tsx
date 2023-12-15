d4366a03f5a5e0484996bfe186a4bc9517e74520
horiyu <horiyu.dev@gmail.com>
2023-12-15 14:32:58 +0900

    add: icon at header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import styles from './header.module.css'
 import OutlinedButton from '../Button/Outlined'
 import { signIn, signOut } from 'next-auth/react'
@@ -9,9 +10,18 @@ export default function Header({ session }: { session: Session | null }) {
   return (
     <header className={styles.header}>
       <div>GitGift</div>
-      {(!session) ?
-        <OutlinedButton onClick={() => signIn()}>Sign in</OutlinedButton> :
-        <OutlinedButton onClick={() => signOut()}>Sign out</OutlinedButton>}
+      {(session) ?
+        <Link href={`./${session.user?.name}`}>
+          <img
+            className={styles.icon}
+            src={session.user?.image || ""}
+            alt={session.user?.name || "icon"}
+            style={{ width: "30px", height: "30px" }}
+          />
+        </Link> :
+        // <OutlinedButton onClick={() => signOut()}>{session.user?.name}</OutlinedButton>
+        <OutlinedButton onClick={() => signIn()}>Sign in</OutlinedButton>
+      }
     </header>
   )
 }

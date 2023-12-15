a1fe21e7ce79564a2650b9c99bf5ed4585925f44
horiyu <horiyu.dev@gmail.com>
2023-12-15 13:46:10 +0900

    add: GitHub auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
+// export const dynamic = 'force-static'
+// export const dynamicParams = false
+
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
+'use client'
+
 import styles from './header.module.css'
 import OutlinedButton from '../Button/Outlined'
+import { signIn, signOut } from 'next-auth/react'
+import { Session } from 'next-auth'
 
-export default function Header() {
+export default function Header({ session }: { session: Session | null }) {
   return (
     <header className={styles.header}>
       <div>GitGift</div>
-      <OutlinedButton>Sign in</OutlinedButton>
+      {(!session) ?
+        <OutlinedButton onClick={() => signIn()}>Sign in</OutlinedButton> :
+        <OutlinedButton onClick={() => signOut()}>Sign out</OutlinedButton>}
     </header>
   )
 }
